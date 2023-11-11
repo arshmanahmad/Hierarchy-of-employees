c4574497d55152f49989237ed303cea4e71fbd2b
arshmanahmad <132290386+arshmanahmad@users.noreply.github.com>
2023-11-11 09:05:38 -0800

    Initial commit

---
+++ HierarchyOfEmployee.java
@@ -0,0 +1,46 @@
+// Write a Java program that creates a class 
+// hierarchy for employees of a company. The base
+//  class should be Employee, with subclasses 
+//  Manager, Developer, and Programmer.
+//   Each subclass should have properties 
+//   such as name, address, salary, and job 
+//   title. Implement methods for calculating 
+//   bonuses, generating performance reports,
+//    and managing 
+// projects
+
+class Employee{
+
+    void bonuses(){
+        
+
+        
+    }
+    void reports(){
+
+    }
+    void managing(){
+
+    }
+}
+class Manager{
+    String name;
+    String address;
+    double salary;
+    String jobTitle;
+    
+}
+class Developer{
+    String name;
+    String address;
+    double salary;
+    String jobTitle;
+    
+}
+class Programmer{
+    String name;
+    String address;
+    double salary;
+    String jobTitle;
+
+}
